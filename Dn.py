e5142c32d0501ad402733c59c5cf2631b2c69ef0
mobiusjoe314 <jmcewen314@gmail.com>
2016-07-06 14:06:46 -0400

    added dn routine

---
+++ Dn.py
@@ -0,0 +1,100 @@
+import numpy as np
+from numpy import pi 
+from hmf import ST_hmf
+import sys
+
+def volume(r1,r2,Theta,Phi):
+    phi1,phi2=Phi
+    theta1,theta2=Theta
+    result=(phi2-phi1)*(np.cos(theta1)- np.cos(theta2))*(r2**3-r1**3)
+    return result 
+
+class DO_n:
+    def __init__(self, n_obs,zbins,mass,CosmoPie,basis,geo): 
+        # the n_obs_dict should contain
+        # the following information:
+        # number of objects in z-bins
+        # the sky coverage \Omega_s 
+        # mass is the cutoff mass, will consider the number of objects below mass=mass
+        
+     
+        self.mf=ST_hmf(CosmoPie)
+        n_zbins=zbins.size
+        
+        n1=n_obs[0]
+        n2=n_obs[1]
+        Theta=geo[0]; Phi=geo[1]
+        print n1, n2, zbins, Theta, Phi
+          
+        V1=np.zeros(n_zbins-1)
+        
+        r_min=np.zeros(n_zbins-1)
+        r_max=np.zeros(n_zbins-1)
+        for i in range(1,n_zbins):
+            z1=zbins[i-1]
+            z2=zbins[i]
+            r1=CosmoPie.D_comov(z1)
+            r2=CosmoPie.D_comov(z2)
+            r_min[i-1]=r1
+            r_max[i-1]=r2
+            V1[i-1]=volume(r1,r2,Theta,Phi)
+            
+        
+        V2=np.zeros_like(V1)
+        V2=V1
+        bias=np.zeros_like(V1)
+        n_avg=np.zeros_like(V1)
+        
+        d_delta=np.zeros(n_zbins-1,dtype=object)
+        for i in range(n_zbins):
+            
+            if i > 0:
+
+                z_avg=(zbins[i]+zbins[i-1])/2.
+                bias[i-1]=self.mf.bias(mass,z_avg)
+                n_avg[i-1]=self.mf.dndM(mass,z_avg)
+
+                d_delta[i-1]=basis.D_delta_bar_D_delta_alpha(r_min[i-1],r_max[i-1],Theta,Phi)
+            
+        self.DO=(n1/V1 - n2/V2)/n_avg**2*bias*d_delta
+        
+        def dn_ddelta(self):
+            return self.DO
+                
+        
+
+        
+if __name__=="__main__":
+    print 'hello'
+    
+    from defaults import cosmology 
+    from cosmopie import CosmoPie
+    d=np.loadtxt('Pk_Planck15.dat')
+    k=d[:,0]; P=d[:,1]
+    
+    Omega_s1=18000
+    z_bins1=np.array([.1,.2,.3])
+    N1=np.array([1e6, 5*1e7]) 
+   
+    
+    S1={'name':'LSST', 'sky_frac': Omega_s1, 'z_bins': z_bins1, 'N': N1}
+    
+    Omega_s2=18000/2.
+    z_bins2=np.array([.1,.2,.35,.5])
+    N2=np.array([1e7, 5*1e7]) 
+  
+    S2={'name':'WFIRST', 'sky_frac': Omega_s2, 'z_bins': z_bins2, 'N': N2}
+    
+    n_obs_dict=[S1, S2]
+    
+    cp=CosmoPie(cosmology, k=k, P_lin=P)
+    r_max=cp.D_comov(4)
+    l_alpha=np.array([1,2,3,4,5])
+    n_zeros=3
+    
+    from sph import sph_basis
+    geo=sph_basis(r_max,l_alpha,n_zeros,k,P)
+    
+    O=DO_n(n_obs_dict,1e15,cp,geo)
+    
+    
