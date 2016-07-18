fixed normalization

Dn.py
@@ -6,7 +6,7 @@ import sys
 def volume(r1,r2,Theta,Phi):
     phi1,phi2=Phi
     theta1,theta2=Theta
-    result=(phi2-phi1)*(np.cos(theta1)- np.cos(theta2))*(r2**3-r1**3)/3. #should be this 3
+    result=(phi2-phi1)*(np.cos(theta1)- np.cos(theta2))*(r2**3-r1**3)/3. 
     return result 
 
 class DO_n:
@@ -57,16 +57,17 @@ class DO_n:
                 d_delta[i-1]=basis.D_delta_bar_D_delta_alpha(r_min[i-1],r_max[i-1],Theta,Phi)
         
         print ' dn number n1, n2, n_avg, bias', n1/V1, n2/V2, n_avg, bias 
-        #sys.exit()
-            
+        print ' v1,v2', V1,V2    
+
         self.DO=(n1/V1 - n2/V2)/n_avg**2*bias*d_delta
         
-        self.N_ab=1/n_avg*(1/V1 + 1/V2)  # inverse of N_ab
+        print self.DO
+        self.N_ab=1/n_avg*(1/V1 + 1/V2)  #  N_ab
         
         self.F_alpha_beta=np.zeros(zbins.size-1,dtype=object)
         for i in range(zbins.size-1):
             v=self.DO[i]
-            self.F_alpha_beta[i]=np.outer(v,v)*self.N_ab[i]
+            self.F_alpha_beta[i]=np.outer(v,v)*1./self.N_ab[i]
            
         
     def dn_ddelta(self):

Super_Survey.py
@@ -7,7 +7,9 @@ import sph_basis as basis
 from sph import sph_basis
 from time import time 
 from Dn import DO_n
+import Dn
 import shear_power as sh_pow
+from hmf import ST_hmf
 
 import sys
 from time import time
@@ -81,8 +83,8 @@ class super_survey:
 		self.O_I_data=self.get_O_I(k,P_lin)
 		self.F_0=self.basis.get_F_alpha_beta()
 			
-		self.cov_mit=self.get_SSC_covar(mitigation=True)	
-		self.cov_no_mit=self.get_SSC_covar(mitigation=False)	
+		self.cov_mit,self.a_mit=self.get_SSC_covar(mitigation=True)	
+		self.cov_no_mit,self.a_no_mit=self.get_SSC_covar(mitigation=False)	
 		
 		t2=time()
 		print 'all done'
@@ -93,8 +95,10 @@ class super_survey:
 	    
 	    result=np.zeros_like(2,dtype=object)
             if mitigation:
+                print "mit"
 	        x=self.O_a_data[0]
             else:
+                print "no mit"
                 x=np.zeros(2)
 	    #print x[0]
 	    #print self.F_0
@@ -102,30 +106,33 @@ class super_survey:
 	    #sys.exit()
 	    F_1=x[0] + self.F_0
 	    F_2=x[1] + self.F_0
+            print "x[0]: ",x[0]
             print "F_1: ",F_1
 	    C_1=np.linalg.pinv(F_1)
 	    C_2=np.linalg.pinv(F_2)
-	    
-	    print C_1 
-	    print C_1.shape, C_2.shape
+           	    
+	    #print C_1 
+	    #print C_1.shape, C_2.shape
 	    C=np.array([C_1,C_2],dtype=object)
 	    Cov_SSC = np.zeros((2,2),dtype=object) 
+	    a_SSC = np.zeros((2,2)) 
 	    for i in range(2):
 	        x=self.O_I_data[i]
 	        T=x['shear_shear']['ddelta_dalpha']
 	        dCdbar=x['shear_shear']['dc_ddelta']
 	        for j in range(2):
-	            a=np.dot(T[j],np.dot(C[j],T[j]))
-	            #print T[j]
-                    #print C[j]
-                    print "a is: ",a 
-	            Cov_SSC[i,j]=np.outer(dCdbar[j],dCdbar[j])*a 
-	            print Cov_SSC[i,j]
-	            print Cov_SSC[i,j].shape
+	            a_SSC[i,j]=np.dot(T[j],np.dot(C[j],T[j]))
+	            print "max t",max(T[j])
+                    print "max C",np.max(C[j])
+
+                    print "a is: ",a_SSC[i,j]
+	            Cov_SSC[i,j]=np.outer(dCdbar[j],dCdbar[j])*a_SSC[i,j]
+	            #print Cov_SSC[i,j]
+	            #print Cov_SSC[i,j].shape
 	          #  np.savetxt('covar_SCC_0.dat',C_SSC)
 	        #    sys.exit()
 	    #sys.exit()
-	    return Cov_SSC
+	    return Cov_SSC,a_SSC
 	def get_O_a(self):
 		D_O_a=np.zeros(self.N_O_a,dtype=object)
 		print 'I have this many long wavelength observables', self.N_O_a 
@@ -141,6 +148,7 @@ class super_survey:
 		            print n_obs, mass
 		            X=DO_n(n_obs,self.zbins_lw,mass,self.CosmoPie,self.basis,self.geo_lw)
 		            D_O_a[i] = X.Fisher_alpha_beta()
+                            print D_O_a[i]
 		# x=D_O_a[0]
 # 		print x[0]
 # 		print x[1]
@@ -175,6 +183,7 @@ class super_survey:
                             sp2 = sh_pow.shear_power(k,self.CosmoPie,zs,ls,P_in=P_lin,pmodel='halofit_nonlinear')
 
                             dcs = np.zeros((z_bins.size-1,ls.size))
+                            cs = np.zeros((z_bins.size-1,ls.size))
                             #covs = np.array([],dtype=object)
 
                             #sh_pows = np.array([],dtype=object)
@@ -182,6 +191,7 @@ class super_survey:
                                 chi_min = self.CosmoPie.D_comov(z_bins[j])
                                 chi_max = self.CosmoPie.D_comov(z_bins[j+1])
                                 #sh_pow2 = sh_pow.Cll_sh_sh(sp2,chi_max,chi_max,chi_min,chi_min).Cll()
+                                cs[j] = sh_pow.Cll_sh_sh(sp2,chi_max,chi_max,chi_min,chi_min).Cll()
                                 dcs[j] = sh_pow.Cll_sh_sh(sp1,chi_max,chi_max,chi_min,chi_min).Cll()
                                 ddelta_dalpha[j]=self.basis.D_delta_bar_D_delta_alpha(chi_min,chi_max,Theta,Phi)
                                 #print ddelta_dalpha[j]
@@ -190,6 +200,7 @@ class super_survey:
 		            
                              #   covs = np.append(covs,np.diagflat(sp2.cov_g_diag(sh_pow2,sh_pow2,sh_pow2,sh_pow2))) #TODO support cross z_bin covariance correctly
                             covs = sp2.cov_mats(z_bins,cname1='shear',cname2='shear')
+                            result[i][key]['power'] = cs
                             result[i][key]['dc_ddelta'] = dcs
                             result[i][key]['covariance'] = covs
                             result[i][key]['ddelta_dalpha']=ddelta_dalpha
@@ -204,13 +215,16 @@ class super_survey:
 		
 if __name__=="__main__":
 
-	z_max=4.0; l_max=20 
-	cp=CosmoPie()
+	z_max=0.5; l_max=20 
+	#d=np.loadtxt('Pk_Planck15.dat')
+        d=np.loadtxt('camb_m_pow_l.dat')
+	k=d[:,0]; P=d[:,1]
+	cp=CosmoPie(k=k,P_lin=P)
 	r_max=cp.D_comov(z_max)
 	print 'this is r max and l_max', r_max , l_max
 	
-	Theta=[np.pi/4,np.pi/2.]
-	Phi=[0,np.pi/3.]
+	Theta=[np.pi/4.,np.pi/2.]
+	Phi=[0.,np.pi/3.]
 	geo=np.array([Theta,Phi])
 	zbins=np.array([.1,.2,.3])
 	l=np.logspace(np.log10(2),np.log10(3000),1000)
@@ -221,10 +235,22 @@ if __name__=="__main__":
 	O_I1={'shear_shear':shear_data1}
 	O_I2={'shear_shear':shear_data2}
 	
-	n_dat1=np.array([5.*1e5/1.7458,5.0*1e5*1.4166*0.98546])
-	n_dat2=np.array([1.01*5.*1e5/1.7458,1.01*5.0*1e5*1.4166*0.98546])
+
+	#n_dat2=np.array([1.01*5.*1e5/1.7458,1.01*5.0*1e5*1.4166*0.98546])
 	M_cut=10**(12.5)
 	
+        n_avg = np.zeros(2)
+        n_avg[0] = ST_hmf(cp).n_avg(M_cut,0.15)
+        n_avg[1] = ST_hmf(cp).n_avg(M_cut,0.25)
+
+        V1 = Dn.volume(cp.D_comov(0.1),cp.D_comov(0.2),Theta,Phi)
+        V2 = Dn.volume(cp.D_comov(0.2),cp.D_comov(0.3),Theta,Phi)
+        #amplitude of fluctuations within a given radius should be given by something like np.sqrt(np.trapz((3.*(sin(k*R)-k*R*cos(k*R))/(k*R)**3)**2*P*k**2,k)*4.*np.pi/(2.*np.pi)**3), where R is the radius of the bin, R=cp.D_comov(z[i])-cp.D_comov(z[i-1])
+        #cf https://ned.ipac.caltech.edu/level5/March01/Strauss/Strauss2.html
+        #~0.007 for R~400 (check units).
+	n_dat1=np.array([n_avg[0]*V1,n_avg[1]*V2])
+	n_dat2=np.array([1.1*n_avg[0]*V1,1.1*n_avg[1]*V2])
+
 	O_a={'number density':np.array([n_dat1,n_dat2,M_cut])}
 	
 	d_1={'name': 'survey 1', 'area': 18000}
@@ -240,13 +266,23 @@ if __name__=="__main__":
 	survey_3={'details':d_3, 'O_a':O_a, 'zbins':zbins,'geo':geo}
 	surveys_lw=np.array([survey_3])
 	
-	d=np.loadtxt('Pk_Planck15.dat')
-	k=d[:,0]; P=d[:,1]
-	l=np.arange(0,5)
-	n_zeros=3
+        l=np.arange(0,5)
+	n_zeros=8
 	
 	print 'this is r_max', r_max 
 	
 	SS=super_survey(surveys_sw, surveys_lw,r_max,l,n_zeros,k,P_lin=P)
 	
-	
+        print "fractional mitigation: ", SS.a_no_mit/SS.a_mit	
+
+        import matplotlib.pyplot as plt
+        ax = plt.subplot(111)
+	l=np.logspace(np.log10(2),np.log10(3000),1000)
+        cov_ss = np.diag(SS.O_I_data[0]['shear_shear']['covariance'][0,0])
+        c_ss = SS.O_I_data[0]['shear_shear']['power'][0]
+        ax.loglog(l,cov_ss)
+        #ax.loglog(l,np.diag(SS.cov_mit[0,0])/c_ss**2*l/2)
+        ax.loglog(l,(np.diag(SS.cov_mit[0,0])))
+        ax.loglog(l,(np.diag(SS.cov_no_mit[0,0])))
+        ax.legend(['cov','mit','no mit'])
+        plt.show()

shear_power.py
@@ -23,9 +23,12 @@ class shear_power:
         self.zs = zs
         self.ls = ls
         self.epsilon = 0.0001
-        self.delta_l = ls[-1]-ls[0] #maybe not right
-        self.omega_s = 5000 #filler, from eifler, in deg^2
-        self.n_gal = 10.*3600. #filler, from krause & eifler in galaxies/deg^2 suggested at possible result from DES
+       # self.delta_l = ls[-1]-ls[0] #maybe not right
+     #   self.omega_s = 5000 #filler, from eifler, in deg^2
+     #   self.n_gal = 10.*3600. #filler, from krause & eifler in galaxies/deg^2 suggested at possible result from DES
+        self.delta_l = 1.
+        self.omega_s = np.pi/(3.*np.sqrt(2))
+        self.n_gal = 286401.
 
         self.pmodel = pmodel
         self.sigma2_e = 0.32 #from eifler
@@ -518,8 +521,8 @@ if __name__=='__main__':
     
 	C=cp.CosmoPie(cosmology=defaults.cosmology)
         #d_t = np.loadtxt('chomp_pow_nlin.dat')
-	d = np.loadtxt('Pk_Planck15.dat')
-#	d = np.loadtxt('camb_m_pow_l.dat')
+#	d = np.loadtxt('Pk_Planck15.dat')
+	d = np.loadtxt('camb_m_pow_l.dat')
 #	xiaod = np.loadtxt('power_spectrum_1.dat')
 #	intd = pickle.load(open('../CosmicShearPython/Cll_camb.pkl',mode='r'))
         #chompd = np.loadtxt('sh_sh_comp.dat')
@@ -543,11 +546,12 @@ if __name__=='__main__':
 	#ls = chompd[:,0]	
 #	ls = revd[:,0]	
 	t1 = time()
-        cosmo_a = defaults.cosmology.copy()
-        #k_a = d[:,0]
-        #P_a = d[:,1]
-        k_a,P_a = cpow.camb_pow(cosmo_a)
+        #cosmo_a = defaults.cosmology.copy()
+        k_a = d[:,0]
+        P_a = d[:,1]
+        #k_a,P_a = cpow.camb_pow(cosmo_a)
        # P_a = hf.halofitPk(k_a,C=C).D2_L(k_a,0)
+      
         zmin = 0.40
         zmax = 0.41
         chimin = C.D_comov(zmin)
@@ -580,9 +584,9 @@ if __name__=='__main__':
         #p3a = sp3a.p_dd_use[:,40]
         #p3b = sp3b.p_dd_use[:,40]
         zbar = 3.
-        #dcalt1,p1a = dp_ddelta(k_a,P_a,zbar,pmodel='linear')
-        #dcalt2,p2a = dp_ddelta(k_a,P_a,zbar,pmodel='halofit')
-        #dcalt3,p3a = dp_ddelta(k_a,P_a,zbar,pmodel='fastpt')
+        dcalt1,p1a = dp_ddelta(k_a,P_a,zbar,pmodel='linear')
+        dcalt2,p2a = dp_ddelta(k_a,P_a,zbar,pmodel='halofit')
+        dcalt3,p3a = dp_ddelta(k_a,P_a,zbar,pmodel='fastpt')
 #        p1a = P_a**C.G_norm(zbar)**2
         #p1b = hf.halofitPk(k_a,P_a*(1.+epsilon/C.sigma8)**2,C=C).D2_NL(k_a,0.4)
 #        dpdk1 =(InterpolatedUnivariateSpline(k_a,p1a,ext=2).derivative(1))(k_a) 
@@ -665,11 +669,11 @@ if __name__=='__main__':
 #        sp12c = shear_power(k_a,C,zs,ls,pmodel='halofit_var_redshift',P_in=np.array([P_a,P_c]),cosmology_in=defaults.cosmology,P_select=P_select)
 #        sh_pow12c = sp12c.Cll_sh_sh()
 #        div_c4 = abs(sh_pow12-2.*sh_pow8+sh_pow12c)/(2.*epsilon)
-       # sp7 = shear_power(k_in,C,zs,ls,pmodel='cosmosis_nonlinear',P_in=d[:,1],cosmology_in=defaults.cosmology)
-      #  sh_pow7 = sp7.Cll_sh_sh()
-   #     sh_pow7_gg = sp7.Cll_g_g()
-   #     sh_pow7_sg = sp7.Cll_sh_g()
-   #     sh_pow7_mm = sp7.Cll_mag_mag()
+#        sp7 = shear_power(k_in,C,zs,ls,pmodel='cosmosis_nonlinear',P_in=d[:,1],cosmology_in=defaults.cosmology)
+        #sh_pow7 = Cll_sh_sh(sp7).Cll()
+        #sh_pow7_gg = Cll_g_g(sp7).Cll()
+        #sh_pow7_sg = Cll_sh_g(sp7).Cll()
+        #sh_pow7_mm = Cll_mag_mag(sp7).Cll()
   #      sp5 = shear_power(k_in,C,zs[0:50],ls,pmodel='halofit_nonlinear',P_in=d[:,1],cosmology_in=defaults.cosmology)
    #     sh_pow5 = sp5.Cll_sh_sh()
     #    sp6 = shear_power(k_in,C,zs,ls,pmodel='halofit_nonlinear',P_in=d[:,1],cosmology_in=defaults.cosmology)
@@ -683,9 +687,9 @@ if __name__=='__main__':
         #import projected_power as prj
         #pp=prj.projected_power(k_in,d[:,1],C,3)
         #C_EE1=pp.C_EE(3,ls)
-#        sh_sh_pow = sp7.Cll_sh_sh()
-#        sh_g_pow = sp7.Cll_sh_g()
-#        g_g_pow = sp7.Cll_g_g()
+#        sh_sh_pow = Cll_sh_sh(sp7).Cll()
+#        sh_g_pow = Cll_sh_g(sp7).Cll()
+#        g_g_pow = Cll_g_g(sp7).Cll()
 #
 #        n_ss = sp7.sigma2_e/(2.*sp7.n_gal)
 #        n_gg = 1/sp7.n_gal
@@ -695,7 +699,7 @@ if __name__=='__main__':
 #        #ac,ad,bd,bc
 #        cov_ss_gg = sp7.cov_g_diag(sh_g_pow,sh_g_pow,sh_g_pow,sh_g_pow)
 #        cov_sg_sg = sp7.cov_g_diag(sh_sh_pow,sh_g_pow,g_g_pow,sh_g_pow,n_ss,0,n_gg,0)
-#      #  cov_sg_sg2 = sp7.cov_g_diag2([q_shear(sp7),q_num(sp7),q_shear(sp7),q_num(sp7)],[n_ss,0,n_gg,0],r_bd=sp7.r_corr(),r_bc=sp7.r_corr())
+      #  cov_sg_sg2 = sp7.cov_g_diag2([q_shear(sp7),q_num(sp7),q_shear(sp7),q_num(sp7)],[n_ss,0,n_gg,0],r_bd=sp7.r_corr(),r_bc=sp7.r_corr())
 #        cov_sg_ss = sp7.cov_g_diag(sh_sh_pow,sh_sh_pow,sh_g_pow,sh_g_pow,n_ss,n_ss,0,0)
 #        cov_sg_gg = sp7.cov_g_diag(sh_g_pow,sh_g_pow,g_g_pow,g_g_pow,0,0,n_gg,n_gg)
 #        cov_gg_gg = sp7.cov_g_diag(g_g_pow,g_g_pow,g_g_pow,g_g_pow,n_gg,n_gg,n_gg,n_gg)
@@ -710,21 +714,22 @@ if __name__=='__main__':
     	ax.set_xlabel('l',size=20)
         plt.title('gaussian source with $\sigma=0.4$ centered at $z=1.0$')
         ax.set_ylabel('|$\\frac{\partial ln(P(k))}{\partial \\bar{\delta}}$|')
-        ax.loglog(ls,dc_lin)
-        ax.loglog(ls,dc_hf)
-        ax.loglog(ls,dc_fpt)
+#        ax.loglog(ls,cov_ss_gg)
+       # ax.loglog(ls,dc_lin)
+       # ax.loglog(ls,dc_hf)
+       # ax.loglog(ls,dc_fpt)
         #ax.loglog(ls,abs(dcnl))
         #ax.loglog(ls,abs(deltabar3))
         #ax.loglog(ls,abs(dclin))
         #ax.loglog(ls,abs(dcalt))
-#        ax.plot(k_a,abs(dcalt1/p1a))
-#        ax.plot(k_a,abs(dcalt2/p2a))
-#        ax.plot(k_a,abs(dcalt3/p3a))
+        ax.plot(k_a,abs(dcalt1/p1a))
+        ax.plot(k_a,abs(dcalt2/p2a))
+        ax.plot(k_a,abs(dcalt3/p3a))
         #ax.loglog(k_a,abs(dcalt1/p1a))
         #ax.loglog(k_a,abs(dcalt2/p2a))
         #ax.loglog(k_a,abs(dcalt3/p3a))
- #       plt.xlim([0.,0.4])
-  #      plt.ylim([1.2,3.2])
+        plt.xlim([0.,0.4])
+        plt.ylim([1.2,3.2])
         #ax.loglog(ls,abs(dcalt3))
         ax.legend(['Linear','Halofit','1 loop fpt'],loc=3)
 #	ax.set_ylabel('l(l+1)$C^{\gamma\gamma}(2\pi)^{-1}$')
@@ -801,5 +806,5 @@ if __name__=='__main__':
  #       ax.loglog(d[:,0],d[:,1]*C.G_norm(0.429)**2)
        # ax.loglog(d_t[:,0],sp2.q_shear()*d_t[0,1]/sp2.q_shear()[0])
         plt.grid()
-	plt.show()
+	#plt.show()
        # np.savetxt('chis_save.dat',sp3.chis)

sph.py
@@ -14,8 +14,10 @@ def I_alpha(k_alpha,k,r_max,l_alpha):
 			
 			a=k_alpha*r_max; b=k*r_max 
 			l=l_alpha+.5
+          #              print pi/2./sqrt(k_alpha*k)/(k_alpha**2 - k**2)*r_max*(k*jv(l-1,b)*jv(l,a)-k_alpha*jv(l-1,a)*jv(l,b))
+                        return pi/2./sqrt(k_alpha*k)/(k_alpha**2 - k**2)*r_max*(-k_alpha*jv(l-1,a)*jv(l,b))
 		#	return pi/2./sqrt(k_alpha*k)/(k_alpha*82 - k**2)*r_max*(k*jv(l-1,b)*jv(l,a)-k_alpha*jv(l-1,a)*jv(l,b))
-			return pi/2./sqrt(k_alpha*k)/(k_alpha**2 - k**2)*r_max*(k*jv(l-1,b)*jv(l,a)-k_alpha*jv(l-1,a)*jv(l,b))
+	#		return pi/2./sqrt(k_alpha*k)/(k_alpha**2 - k**2)*r_max*(k*jv(l-1,b)*jv(l,a)-k_alpha*jv(l-1,a)*jv(l,b))
 		#	print  pi/2.*r_max**2/sqrt(k_alpha*k)*(a*jv(l,b)*dJ_n(l,a)-b*jv(l,a)*dJ_n(l,b))/(b**2-a**2)
 		#	print pi/2./sqrt(k_alpha*k)*r_max*(k_alpha*jv(l,b)*dJ_n(l,a)-k*jv(l,a)*dJ_n(l,b))/(k**2-k_alpha**2)
 			
@@ -38,7 +40,7 @@ class sph_basis(object):
 		k,P_lin=CosmoPie.get_P_lin()
 		
 		self.l=l_alpha
-		
+	        self.r_max = r_max	
 		# define the super mode wave vector k alpha
 		# and also make the map from l_alpha to k_alpha 
 		
@@ -80,13 +82,13 @@ class sph_basis(object):
 		            self.C_id[id,2]=mm[c]
                             self.Is[id] = I_alpha(kk[b],k,r_max,ll)
 		            id=id+1
+                        #calculate I integrals and make table
                         for b in range(0,kk.size):
                             A = self.Is[id_k1+b]
                             for d in range(b,kk.size):
                                 B = self.Is[id_k1+d]
-                                self.C_alpha_beta[id_k1+b,id_k1+d]=2./np.pi*trapz(k**2*P_lin*A*B,k)*(-1.)**ll; #check coefficient
+                                self.C_alpha_beta[id_k1+b,id_k1+d]=2./(np.pi*self.norm_factor(kk[b],ll)*self.norm_factor(kk[d],ll))*trapz(k**2*P_lin*A*B,k)*(-1.)**ll; #check coefficient
                                 self.C_alpha_beta[id_k1+d,id_k1+b]=self.C_alpha_beta[id_k1+b,id_k1+d];
-		            	    	    	    
 	        #TODO can make more efficient if necessary	
                 t2 = time()
                 print "basis time: ",t2-t1
@@ -103,7 +105,13 @@ class sph_basis(object):
 		#            #self.C_alpha_beta[alpha,beta]=8.*trapz(k**2*P_lin*A*B,k)
 		#            self.C_alpha_beta[alpha,beta]=2./np.pi*trapz(k**2*P_lin*A*B,k)*(-1)**l1;
 		#            
+                print "max c 1",np.max(self.C_alpha_beta)
 		self.F_alpha_beta=np.linalg.inv(self.C_alpha_beta)
+
+        #I_\alpha(k_\alpha,r_{max}) simplified
+        def norm_factor(self,ka,la):
+            return -np.pi*self.r_max**2/(4.*ka)*jv(la+1.5,ka*self.r_max)*jv(la-0.5,ka*self.r_max)
+
 	def Cov_alpha_beta(self):
 		return self.C_alpha_beta
 	
@@ -122,10 +130,12 @@ class sph_basis(object):
 		
 		theta_min, theta_max=Theta
 		phi_min, phi_max=Phi
-		def integrand(theta,phi):
+		#def integrand(theta,phi):
+		def integrand(phi,theta):
 			#print theta, phi 
 			return sin(theta)*Y_r(l,m,theta,phi)
 	
+		#I = dblquad(integrand,theta_min,theta_max, lambda phi: phi_min, lambda phi :phi_max)[0]
 		I = dblquad(integrand,theta_min,theta_max, lambda phi: phi_min, lambda phi :phi_max)[0]
 				
 		if (np.absolute(I) <= eps):
@@ -140,7 +150,8 @@ class sph_basis(object):
 
 		def integrand(r):
 			return r**2*j_n(n,r*k)
-
+                #CHANGED
+		#I= quad(integrand,r_min,r_max)[0]
 		I= quad(integrand,r_min,r_max)[0]
 		
 		if (np.absolute(I) <= eps):
@@ -151,9 +162,12 @@ class sph_basis(object):
 	def D_delta_bar_D_delta_alpha(self,r_min,r_max,Theta,Phi):
 	    	    
 	    r=np.array([r_min,r_max])
+            #TODO Check this
 	    a_00=self.a_lm(Theta,Phi,0,0)
-	    Omega=a_00*np.sqrt(4*pi) 
-	    norm=3/(r_max**3 - r_min**3)/Omega
+	    Omega=np.sqrt(a_00)*4.*np.pi
+            #CHANGED
+	   # Omega=a_00*np.sqrt(4*pi) 
+	    norm=3./(r_max**3 - r_min**3)/Omega
 	
 	    result=np.zeros(self.C_id.shape[0])
             #store alms for current l, m pair because computing them is slow
@@ -165,12 +179,12 @@ class sph_basis(object):
 	        kk=self.C_id[id,1]
 	        mm=self.C_id[id,2]
 	        if ll_last == ll and mm_last == mm:
-                    result[id] = self.R_int(r,kk,ll)*alm_last*norm
+                    result[id] = self.R_int(r,kk,ll)*alm_last*norm*self.norm_factor(kk,ll)
                 else:
                     alm_last = self.a_lm(Theta,Phi,ll,mm)
                     ll_last = ll
                     mm_last = mm
-                    result[id]=self.R_int(r,kk,ll)*alm_last*norm
+                    result[id]=self.R_int(r,kk,ll)*alm_last*norm*self.norm_factor(kk,ll)
 
 	    return result
 	    